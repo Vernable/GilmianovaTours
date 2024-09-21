176de612ae31bb6f1dbcbbbd3dde14f20d0e6cb5
Vernable <melkaya.elka26@yandex.ru>
2024-09-21 19:10:58 +0300

    добавили свойство появления

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,5 +32,15 @@ namespace GilmianovaTours
         {
             Manager.MainFrame.GoBack(); 
         }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if(MainFrame.CanGoBack) {
+                BtnBack.Visibility = Visibility.Visible;
+        }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
     }
 }
