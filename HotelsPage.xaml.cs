4743d4588a113cb82dc4f67f813133eca0c6ad22
Vernable <melkaya.elka26@yandex.ru>
2024-09-21 19:00:26 +0300

    добавила обработчик события кнопнки

--- HotelsPage.xaml.cs
+++ HotelsPage.xaml.cs
@@ -24,5 +24,10 @@ namespace GilmianovaTours
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());  
+        }
     }
 }
